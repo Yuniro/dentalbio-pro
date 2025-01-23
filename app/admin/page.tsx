4d298602cae7c478303ecd27a356cdaa052a869b
yuniro <sergey.r1130@gmail.com>
2025-01-23 17:49:52 +0200

    fixed admin middleware

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,29 @@
 import { createClient } from "@/utils/supabase/client";
+import { AdminServer } from "@/utils/functions/useAdminServer";
+import { getEffectiveUserId } from "@/utils/user/getEffectiveUserId";
+import { redirect } from "next/navigation";
 import AdminComponent from "./AdminComponent";
-import { isAdminMiddleware } from "@/utils/user/isAdmin";
 
 const Admin = async () => {
   const supabase = createClient();
+  const { data: authData, error: authError } = await supabase.auth.getUser();
 
-  const isAdmin: boolean = await isAdminMiddleware({ supabase }) || false;
+  if (authError) {
+    return redirect("/login");
+  }
+
+  const { data: userData, error: userError } = await supabase
+    .from("users")
+    .select("username, subscription_status, role")
+    .eq("id", authData.user.id)
+    .single();
+
+  if (!(userData?.subscription_status === "PRO" || userData?.subscription_status === "PREMIUM PRO" || userData?.role === "admin"))
+    return redirect("/dashboard");
 
   return (
     <>
-      {isAdmin && <AdminComponent />}
+      {userData && <AdminComponent />}
     </>);
 };
 
