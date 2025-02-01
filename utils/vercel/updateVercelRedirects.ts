66f349abb8d697b0178ab174a2a799afd97a7fd5
yuniro <sergey.r1130@gmail.com>
2025-02-01 18:33:55 +0200

    fixed vercel url version issue

--- utils/vercel/updateVercelRedirects.ts
+++ utils/vercel/updateVercelRedirects.ts
@@ -2,7 +2,7 @@ export const updateVercelRedirects = async (username: string, domain: string) =>
   const projectId = process.env.NEXT_PUBLIC_VERCEL_PROJECT_ID;
   const vercelToken = process.env.NEXT_PUBLIC_VERCEL_API_KEY;
 
-  const response = await fetch(`https://api.vercel.com/v9/projects/${projectId}/config`, {
+  const response = await fetch(`https://api.vercel.com/v4/projects/${projectId}/config`, {
     method: "PATCH",
     headers: {
       "Authorization": `Bearer ${vercelToken}`,
