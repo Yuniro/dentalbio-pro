052fc10455a442b2e068d156d979cfacbf42b5ac
Yuniro <sergey.r1130@gmail.com>
2025-03-10 09:14:11 +0200

    change getUserLocation function

--- app/upgrade-success/SendEmailConfirmation.tsx
+++ app/upgrade-success/SendEmailConfirmation.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect } from "react";
+import { getUserLocation } from "@/utils/functions/getUserIp";
 
 export default function SendEmailConfirmation({
   email,
@@ -20,20 +21,6 @@ export default function SendEmailConfirmation({
   position: string;
 }) {
   useEffect(() => {
-    // Function to get user's location using ipinfo.io
-    async function getUserLocation() {
-      try {
-        const response = await fetch(
-          `https://ipinfo.io/json?token=${process.env.NEXT_PUBLIC_IPINFO_TOKEN}`
-        );
-        const data = await response.json();
-        return `${data.city}, ${data.region}, ${data.country}`;
-      } catch (error) {
-        console.error("Error fetching location:", error);
-        return "Unknown"; // Default to "Unknown" if location can't be fetched
-      }
-    }
-
     // Function to send the confirmation email
     async function sendConfirmationEmail() {
       // Get user time
--- utils/functions/getUserIp.ts
+++ utils/functions/getUserIp.ts
@@ -4,7 +4,12 @@ export const getUserLocation = async () => {
             `https://ipinfo.io/json?token=${process.env.NEXT_PUBLIC_IPINFO_TOKEN}`
         );
         const data = await response.json();
-        return `${data.city}, ${data.region}, ${data.country}`;
+
+        const regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+        const fullCountryName = regionNames.of(data.country) || data.country;
+
+        return fullCountryName
+        // return `${data.city}, ${data.region}, ${data.country}`;
     } catch (error) {
         console.error("Error fetching location:", error);
         return "Unknown"; // Default to "Unknown" if location can't be fetched
