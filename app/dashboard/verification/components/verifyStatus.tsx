ab78ccda3a3ad6b49e2b0165336a3d7b86b19125
Yuniro <sergey.r1130@gmail.com>
2025-03-25 16:13:08 +0200

    fix veriff

--- app/dashboard/verification/components/verifyStatus.tsx
+++ app/dashboard/verification/components/verifyStatus.tsx
@@ -73,7 +73,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
       {(sessionStatus === "approved") &&
         <>
           <div className="p-6 bg-white rounded-[26px]">
-            <h3 className="text-base">You verified your identity</h3>
+            <h3 className="text-gray-500">You verified your identity</h3>
             <p className="text-gray-500">You've completed an important part of establishing trust in our community.</p>
             <div className="text-gray-500">
 
@@ -83,7 +83,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-4">
-                  <h5 className="text-base mb-1">Connect your phone</h5>
+                  <h6 className="text-gray-500 mb-1">Connect your phone</h6>
                   <p className="font-light text-gray-500">Use your phone to access the verification flow.</p>
                 </div>
               </div>
@@ -94,7 +94,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-4">
-                  <h5 className="text-base mb-1">Show us a ID card photo</h5>
+                  <h6 className="text-gray-500 mb-1">Show us a ID card photo</h6>
                   <p className="font-light text-gray-500">We'll check that your ID card's authenticity.</p>
                 </div>
               </div>
@@ -105,7 +105,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-4">
-                  <h5 className="text-base mb-1">Appear on camera</h5>
+                  <h6 className="text-gray-500 mb-1">Appear on camera</h6>
                   <p className="font-light text-gray-500">To show us it's really you, take a selfie.</p>
                 </div>
               </div>
@@ -113,7 +113,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
               <div className="flex gap-2">
                 <CheckCircle size={36} weight="fill" color="#5046db" />
                 <div className="mt-1">
-                  <h5 className="text-base mb-1">Liveness Detection</h5>
+                  <h6 className="text-gray-500 mb-1">Liveness Detection</h6>
                   <p className="font-light text-gray-500">Follow the instructions to ensure you're completing this in real-time.</p>
                 </div>
               </div>
@@ -124,7 +124,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
       {(sessionStatus === "pending") &&
         <>
           <div className="p-6 bg-white rounded-[26px]">
-            <h3 className="text-base mb-3">Verification in Progress</h3>
+            <h3 className="text-gray-500 mb-3">Verification in Progress</h3>
             <p className="text-gray-500 mb-1">Thank you for submitting your verification details.</p>
             <p className="text-gray-500 mb-1">Our team is currently reviewing your information to ensure everything meets the required standards.</p>
             <p className="text-gray-500 mb-1">You will receive a notification via email as soon as the verification process is completed. If additional information is needed, we will reach out to you directly.</p>
@@ -135,7 +135,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
       {(sessionStatus === "declined") &&
         <>
           <div className="p-6 bg-white rounded-[26px]">
-            <h3 className="text-base">Verification failed</h3>
+            <h3 className="text-gray-500">Verification failed</h3>
             <p className="text-gray-500">Please try again:</p>
             <div className="text-gray-500">
 
@@ -145,7 +145,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-2">
-                  <h5 className="text-base mb-1">Connect via your phone</h5>
+                  <h6 className="text-gray-500  mb-1">Connect via your phone</h6>
                   <p className="font-light text-gray-500">You will need to connect to Veriff via your phone.</p>
                 </div>
               </div>
@@ -156,7 +156,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-2">
-                  <h5 className="text-base mb-1">Upload Your ID Card</h5>
+                  <h6 className="text-gray-500 mb-1">Upload Your ID Card</h6>
                   <p className="font-light text-gray-500">You will be prompted to upload a clear picture of your ID card.</p>
                 </div>
               </div>
@@ -167,7 +167,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1">
-                  <h5 className="text-base mb-1">Take a Selfie</h5>
+                  <h6 className="text-gray-500 mb-1">Take a Selfie</h6>
                   <p className="font-light text-gray-500">After uploading your ID, you’ll need to take a selfie. This selfie will be compared to the photo on your ID to confirm that it’s really you.</p>
                 </div>
               </div>
@@ -175,7 +175,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
               <div className="flex gap-2">
                 <div className="w-8 h-8 aspect-square rounded-full border"></div>
                 <div className="mt-1">
-                  <h5 className="text-base mb-1">Perform Liveness Detection</h5>
+                  <h6 className="text-gray-500 mb-1">Perform Liveness Detection</h6>
                   <p className="font-light text-gray-500">Follow the instructions to ensure you're completing this in real-time.</p>
                 </div>
               </div>
@@ -192,7 +192,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
       {(sessionStatus === "not_started") &&
         <>
           <div className="p-6 bg-white rounded-[26px]">
-            <h3 className="text-base">You haven't verified your identity yet</h3>
+            <h3 className="text-gray-500">You haven't verified your identity yet</h3>
             <p className="text-gray-500">How to verify your identity to keep the community safe for everyone:</p>
             <div className="text-gray-500">
 
@@ -202,7 +202,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-2">
-                  <h5 className="text-base mb-1">Connect via your phone</h5>
+                  <h6 className="text-gray-500 mb-1">Connect via your phone</h6>
                   <p className="font-light text-gray-500">You will need to connect to Veriff via your phone.</p>
                 </div>
               </div>
@@ -213,7 +213,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1 mb-2">
-                  <h5 className="text-base mb-1">Upload Your ID Card</h5>
+                  <h6 className="text-gray-500 mb-1">Upload Your ID Card</h6>
                   <p className="font-light text-gray-500">You will be prompted to upload a clear picture of your ID card.</p>
                 </div>
               </div>
@@ -224,7 +224,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
                   <div className="w-0 border-r flex-grow" />
                 </div>
                 <div className="mt-1">
-                  <h5 className="text-base mb-1">Take a Selfie</h5>
+                  <h6 className="text-gray-500 mb-1">Take a Selfie</h6>
                   <p className="font-light text-gray-500">After uploading your ID, you’ll need to take a selfie. This selfie will be compared to the photo on your ID to confirm that it’s really you.</p>
                 </div>
               </div>
@@ -232,7 +232,7 @@ const VerifyStatus: React.FC<VerifyStatusProps> = ({ enabled }) => {
               <div className="flex gap-2">
                 <div className="w-8 h-8 aspect-square rounded-full border"></div>
                 <div className="mt-1">
-                  <h5 className="text-base mb-1">Perform Liveness Detection</h5>
+                  <h6 className="text-gray-500 mb-1">Perform Liveness Detection</h6>
                   <p className="font-light text-gray-500">Follow the instructions to ensure you're completing this in real-time.</p>
                 </div>
               </div>
