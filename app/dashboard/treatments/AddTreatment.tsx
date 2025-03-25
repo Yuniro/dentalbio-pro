8e7ff5e730bdf155fa1f97aba03ee706f1cd3db1
Yuniro <sergey.r1130@gmail.com>
2025-03-25 09:09:55 +0200

    fix treatment service

--- app/dashboard/treatments/AddTreatment.tsx
+++ app/dashboard/treatments/AddTreatment.tsx
@@ -105,15 +105,10 @@ export default function AddTreatmentForm({ dentistryId }: { dentistryId: string 
         <h2 className="text-lg font-semibold mb-3">Add new {isService ? "service" : "treatment"}</h2>
         <div className="flex justify-between items-center gap-2">
           <label>Treatment</label>
-          <div className="form-check form-switch custom-form-check">
-            <input
-              className="form-check-input cursor-pointer toggler"
-              type="checkbox"
-              role="switch"
-              // id={`flexSwitchCheckChecked-${link.link_id}`}
-              checked={isService}
-              onChange={toggleIsService}
-            />
+          <div className="relative inline-block w-8 h-5">
+            <input checked={isService} onChange={toggleIsService} id="switch-component-blue" type="checkbox" className="peer appearance-none w-8 h-5 bg-[#7d71e5] rounded-full checked:bg-[#7d71e5] cursor-pointer transition-colors duration-300" />
+            <label htmlFor="switch-component-blue" className="absolute top-0 left-0 w-[14px] h-[14px] mt-[3px] bg-[#BFBFBF] rounded-full shadow-sm transition-transform duration-300 peer-checked:translate-x-4 peer-checked:border-grey-500 cursor-pointer">
+            </label>
           </div>
           <label>Service</label>
         </div>
