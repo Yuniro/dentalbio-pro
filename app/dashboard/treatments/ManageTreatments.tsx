52155ceb2b6038c55d5056675d8eceb1d5879d32
Yuniro <sergey.r1130@gmail.com>
2025-03-25 09:54:26 +0200

    fix treat ment error

--- app/dashboard/treatments/ManageTreatments.tsx
+++ app/dashboard/treatments/ManageTreatments.tsx
@@ -84,24 +84,33 @@ const Treatment = ({
       }}
       className="membar-cards p-4"
     >
-      <div className="flex align-items-center gap-3">
+      <div className="flex items-center gap-3">
+        {/* Up/Down Arrows */}
         <div className="flex flex-col gap-0 hover:bg-purple-100 hover:text-[#302a83] rounded-md cursor-pointer p-1">
           <CaretUp className="-mb-0.5" weight="bold" />
           <CaretDown className="-mt-0.5" weight="bold" />
         </div>
-        <div className="w-100">
-          <div className="flex align-items-center justify-content-between gap-2 mb-1 member-cards-actions">
-            <div>
+
+        <div className="w-full">
+          <div className="flex items-center justify-between gap-2 mb-1 member-cards-actions">
+            <div className="max-w-[500px]">
+              {/* Title & Edit Button */}
               <button
                 onClick={() => onEdit(treatment)}
-                className="flex align-items-center gap-2 member-heading"
+                className="flex items-center gap-2 member-heading"
               >
-                <p className="mb-0">{treatment.title}</p>
+                <p className="mb-0 font-semibold text-blue-600 break-words break-all">{treatment.title}</p>
                 <PencilSimple className="w-5 h-5" />
               </button>
-              <p className=" mb-0">{treatment.description}</p>
+
+              {/* Description with word-break fix */}
+              <p className="mb-0 max-w-[500px] break-words break-all">
+                {treatment.description}
+              </p>
             </div>
-            <div className="flex gap-3 align-items-center">
+
+            {/* Delete Button */}
+            <div className="flex gap-3 justify-end items-center">
               <button
                 className="w-10 h-10 p-0.5 hover:bg-red-100 hover:text-red-700 flex items-center justify-center rounded-md transition-all"
                 onClick={() => onDelete(treatment)}
