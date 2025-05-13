9e9ef4ba95e96c6db986f6a29e3f04886d5f2fac
Yuniro <sergey.r1130@gmail.com>
2025-05-13 13:47:36 +0300

    test

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -17,6 +17,7 @@ export async function GET(request: NextRequest) {
 
   if (token_hash && type) {
     const supabase = createClient()
+    console.log(token_hash, '------', type)
 
     const { error } = await supabase.auth.verifyOtp({
       type,
